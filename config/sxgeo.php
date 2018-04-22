8dcba45fcbb27fab6550047c15a017191dc45502
Ruslan A <ruslan.a94@yandex.com>
2018-04-22 13:22:40 +0300

    Add description to Laravel configuration file

--- config/sxgeo.php
+++ config/sxgeo.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+    // URL of countries or cities database file to download and update.
+    // See https://sypexgeo.net/ru/download/.
     'dbFileURL' => 'http://sypexgeo.net/files/SxGeoCity_utf8.zip',
+
+    // The path where the database will be stored.
     'localPath' => database_path('GeoIP.dat'),
 ];
