9c5ce0b2f4792a42d493296e67c9e017d87b16a1
Ruslan /A <ruslan.a94@ya.ru>
2021-08-28 15:17:57 +0300

    Update PHPDoc for Facade

--- src/Facades/SxGeo.php
+++ src/Facades/SxGeo.php
@@ -3,6 +3,14 @@ namespace Eseath\SxGeo\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static int getCountryId(string $ip)
+ * @method static string getCountry(string $ip)
+ * @method static array|false get(string $ip)
+ * @method static array|false getCity(string $ip)
+ * @method static array|false getCityFull(string $ip)
+ * @method static array about()
+ */
 class SxGeo extends Facade
 {
     /**
