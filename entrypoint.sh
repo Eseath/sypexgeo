de86dc64851b5c919d3eeac0e7615b8d462c97a9
Ruslan /A <ruslan.a94@ya.ru>
2021-12-08 11:04:15 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 rm -f composer.lock
 composer require orchestra/testbench:${TESTBENCH} --no-update --no-interaction --dev
 composer install --no-interaction
+echo ""
 composer show | grep laravel
+echo ""
 php --version
+echo ""
+curl --version
+echo ""
 vendor/bin/phpunit
