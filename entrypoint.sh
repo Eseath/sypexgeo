06996ed9db16ec7102d2ba2c10271b5d44d24545
Ruslan /A <ruslan.a94@ya.ru>
2021-12-28 04:52:23 +0300

    Update Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 rm -f composer.lock
-composer require orchestra/testbench:${TESTBENCH} --no-update --no-interaction --dev
+composer require orchestra/testbench:${TESTBENCH_VERSION} --no-update --no-interaction --dev
 composer install --no-interaction
 echo ""
 composer show | grep laravel
