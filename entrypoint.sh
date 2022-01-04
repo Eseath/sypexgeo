604bd3fbcd3b675267732374ad2bcff74b5ea874
Ruslan /A <ruslan.a94@ya.ru>
2022-01-04 22:18:50 +0300

    Add Laravel 5.1-5.4 versions into docker-compose.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,15 @@
 rm -f composer.lock
 composer require orchestra/testbench:${TESTBENCH_VERSION} --no-update --no-interaction --dev
-composer install --no-interaction
+
+if [ ! -z "$PHPUNIT_VERSION" ]; then
+    composer require phpunit/phpunit:${PHPUNIT_VERSION} --no-update --no-interaction --dev
+fi
+
+if [ ! -z "$FAKER_VERSION" ]; then
+    composer require fzaninotto/faker:${FAKER_VERSION} --no-update --no-interaction --dev
+fi
+
+composer install --no-interaction --no-progress --no-ansi
 echo ""
 composer show | grep laravel
 echo ""
